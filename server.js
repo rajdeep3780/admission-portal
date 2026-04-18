b5f63ada25dcf9c6a5030abb0a62f0dd7c054d7b
Rajdeep <rajdeep3780@gmail.com>
2026-04-18 11:46:17 +0530

    fix deploy

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server running...");
-});
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
@@ -49,4 +45,9 @@ app.post("/submit", (req, res) => {
 // Start server
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running...");
 });
