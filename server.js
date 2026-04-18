19595cf607dbfbc511481de18e73aceb7958c383
Rajdeep <rajdeep3780@gmail.com>
2026-04-18 22:35:34 +0530

    fix deploy

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post("/submit", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running...");
